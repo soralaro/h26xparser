96380a93658395dd95c820727e3dab361790f1ff
陈振雄@异构计算 <zhenxiongchen@deepglint.com>
2024-05-31 12:06:51 +0800

    add interface.cpp

---
+++ interface.cpp
@@ -0,0 +1,37 @@
+#include "interface.h"
+H26xParser::H26xParser(){
+    handle_=nullptr;
+}
+H26xParser::~H26xParser(){
+    if(!handle_){
+        h264_parser_destroy(handle_);
+    }
+}
+int H26xParser::init(){
+    handle_ = h264_parser_create();
+}
+
+int H26xParser::getColorRang(u_int8_t *frame,int len){
+    h264_parser_input(handle_,frame,len);
+    if(handle_->ctx._sps->vui_parameters_present_flag > 0){
+            if(handle_->ctx._sps->vui.video_full_range_flag > 0){
+                return 1;
+                //color range full
+            }else{
+                return 0;
+                //color range limited
+            }
+        }else{
+            return -1;
+            //can not parse vui param from h264 stream
+        }
+}
+
+extern "C"{
+    H26xParser *CreatH26xParser(){
+        return new H26xParser;
+    }
+    void DestroyH26xParser(H26xParser *h26xParser){
+        delete h26xParser;
+    }
+}
---
+++ interface.h
@@ -0,0 +1,22 @@
+#ifndef H26XPARSER 
+#define H26XPARSER 
+#include "media/h264/h264-util.h"
+#include "media/h264/h264-parser.h"
+#include "media/h265/h265-sps.h"
+#include <string>
+typedef unsigned char uint8_t;  
+class H26xParser{
+    public:
+    H26xParser();
+    virtual ~H26xParser();
+    int getColorRang(u_int8_t *frame,int len);
+    int init();
+    private:
+    h264_parser_t *handle_;
+};
+extern "C"{
+H26xParser *CreatH26xParser();
+void DestroyH26xParser(H26xParser *h26xParser);
+}
+
+#endif
